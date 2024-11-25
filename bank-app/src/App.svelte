ae9fe87ef5f52d1fbae164f0460e67826fae7353
SonvanJune <sonthu3333@gmail.com>
2024-11-25 16:16:28 +0900

    .

---
+++ bank-app/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import viteLogo from "/vite.svg";
+</script>
+
+<main>
+  <a href="https://vite.dev" target="_blank" rel="noreferrer">
+    <img src={viteLogo} class="logo" alt="Vite Logo" />
+  </a>
+</main>
+
+<style>
+</style>
