c406c2a9983d446b3f985c2076eca8b4333427e6
SonvanJune <sonthu3333@gmail.com>
2024-11-27 15:33:30 +0900

    ..

---
+++ bank-app/src/components/ModalLogin.svelte
@@ -0,0 +1,88 @@
+<script>
+  export let show = false;
+</script>
+
+<div class="modal {show ? 'show' : ''}">
+  <div class="login-group">
+    <a href="/" class="login-link">Digi Bank </a>
+    <div class="login-line"></div>
+    <a href="/" class="login-link">Digi Biz </a>
+    <div class="login-line"></div>
+    <a href="/" class="login-link">iBanking </a>
+    <div class="login-line"></div>
+    <a href="/" class="login-link"
+      >Cashup <div class="login-line"></div></a
+    >
+  </div>
+</div>
+
+<style>
+  .modal {
+    display: none;
+    position: absolute;
+    top: 100%;
+    width: 120%;
+    left: -20%;
+    height: max-content;
+    background-color: rgba(0, 0, 0, 0.5);
+    animation: slideUp 0.4s ease-in-out forwards;
+  }
+
+  @keyframes slideUp {
+    0% {
+      transform: translateY(40px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  .modal.show {
+    display: block;
+  }
+
+  .modal .login-group {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    border-radius: 8px;
+    padding: 0px 5px;
+    width: 100%;
+    height: auto;
+    align-items: center;
+  }
+
+  .modal .login-group .login-link {
+    transition: all 0.3s ease-out 0s !important;
+    padding: 0.7rem 1.75rem;
+    margin-top: 0.2em;
+    margin-bottom: 0.2rem;
+    height: 100%;
+    width: 100%;
+    color: rgba(0, 0, 0, 0.8);
+    font-family: Manrope, sans-serif;
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    white-space: nowrap;
+    display: flex;
+    position: relative;
+    align-items: center;
+    border-radius: 5px;
+  }
+
+  .modal .login-group .login-line {
+    left: 0;
+    border-top: 0.5px solid rgba(0, 0, 0, 0.1);
+    width: 100%;
+    height: 1px;
+    display: block;
+  }
+
+  .modal .login-group .login-link:hover {
+    background-color: rgb(236 251 210);
+  }
+</style>
--- bank-app/src/components/Navbar.svelte
+++ bank-app/src/components/Navbar.svelte
@@ -1,21 +1,134 @@
 <script>
-	import Navbar from './Navbar.svelte';
+  import ModalLogin from "./ModalLogin.svelte";
 
+  let showModal = false;
 </script>
 
 <div class="navbar">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="logo"></div>
-            </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="logo"></div>
+      </div>
+      <div class="col-md-7">
+        <div class="menu">
+          <a class="item active" href="/"
+            >Product&Service<i class="bi bi-caret-down-fill"></i></a
+          >
+          <a class="item" href="/"
+            >Tool&Utilities<i class="bi bi-caret-down-fill"></i></a
+          >
+          <a class="item" href="/"
+            >Contact&Support<i class="bi bi-caret-down-fill"></i></a
+          >
+          <a class="item" href="/">Safe transaction principles</a>
         </div>
+      </div>
+      <div class="col-md-2">
+        <div class="login">
+          <button on:mouseenter={() => showModal = true} on:mouseleave={() => showModal = false}>
+            <i class="bi bi-box-arrow-in-right"></i>
+            <span>Login</span>
+            <ModalLogin show={showModal} />
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 
 <style>
-    .navbar .logo{
-        background-image: url('logo.png');
-        background-size: contain;
-    }
+  .navbar .logo {
+    background-image: url("../../public/img/logo.png");
+    background-size: cover;
+    width: 60px;
+    height: 60px;
+  }
+
+  .navbar .menu {
+    display: flex;
+    justify-content: left;
+    flex-direction: row;
+    height: 100%;
+  }
+
+  .navbar .menu .item {
+    font-family: Manrope, sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1rem;
+    line-height: 1rem;
+    letter-spacing: -0.02em;
+    color: #fff;
+    text-decoration: none;
+    margin-right: 3rem;
+    white-space: nowrap;
+    height: 100%;
+    position: relative;
+    display: flex;
+    justify-content: center; /* Căn giữa theo chiều ngang */
+    align-items: center;
+  }
+
+  .navbar .menu .item:hover {
+    border-bottom: 3px solid #85b94d;
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
+  }
+
+  .navbar .menu .item i {
+    position: absolute;
+    top: 40%;
+    right: -20%;
+  }
+
+  .navbar .login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .row {
+    width: 100% !important;
+  }
+
+  .navbar .login button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.75rem 1.5rem 0.75rem 1rem;
+    z-index: 3;
+    background-color: #a1c038;
+    cursor: pointer;
+    width: 155px;
+    position: relative;
+    order: 1;
+    border: none;
+    font-family: "Manrope SemiBold", sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    letter-spacing: -0.02em;
+    border-radius: 8px;
+    color: rgba(0, 0, 0, 0.8);
+  }
+
+  .navbar .login button:hover {
+    background: linear-gradient(
+        0deg,
+        rgba(255, 255, 255, 0.2),
+        rgba(255, 255, 255, 0.2)
+      ),
+      #a1c038;
+  }
+
+  .navbar .login button i {
+    margin-right: 0.75rem;
+    font-size: 1.4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
